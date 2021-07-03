3aa62a1150b09eeec05d1318667e0a1188808f2a
Shailendra <hereshailendra.com>
2021-07-03 09:14:31 +0530

    nn

---
+++ app/src/main/java/com/example/aaratech/splashscrn.java
@@ -0,0 +1,15 @@
+package com.example.aaratech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashscrn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscrn);
+//        https://career-finder.aaratechnologies.in/job/api/signUp
+    }
+}
